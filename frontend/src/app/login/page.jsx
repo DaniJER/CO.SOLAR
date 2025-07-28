981a26238a5c8d89b7cbb9e623e13ae5f1f5ede8
Daniel Enriquez <danicraft.op7@gmail.com>
2025-07-28 15:15:09 -0500

    upload latest changes

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -3,7 +3,7 @@ import FormLogin from "@/components/forms/LoginForm";
 import NotFound from "../not-found/NotFound";
 
 const Login = () => {
-  return <FormLogin />;
+  return <NotFound />;
 };
 
 export default Login;
